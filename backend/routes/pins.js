57e5d2be855372549b3352588e882f2822088a8d
Saksham Gupta <sakshamg1304@gmail.com>
2023-11-13 17:35:51 +0530

    uploading image to db is working fine

--- backend/routes/pins.js
+++ backend/routes/pins.js
@@ -1,25 +1,51 @@
 const router = require("express").Router();
 const Pin = require("../models/Pin");
 
-//create a pin
+const multer = require('multer');
 
-router.post("/",async(req,res)=>{
-    const newPin = new Pin(req.body);
-    try{
-        const savedPin = await newPin.save(); 
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        // Store the uploaded files in the `uploads/` directory
+        cb(null, 'uploads/');
+    },
+    filename: (req, file, cb) => {
+        // Generate a unique filename for the uploaded file
+        cb(null, `${Date.now()}-${file.originalname}`);
+    }
+});
+
+const upload = multer({ storage: storage });
+
+//create a pin
+router.post("/", upload.array('images'), async (req, res) => {
+    try {
+        const { username, title, desc, rating, lat, long } = req.body;
+        const images = req.files;
+
+        const newPin = new Pin({
+            username,
+            title,
+            desc,
+            rating,
+            lat,
+            long,
+            images: images.map(file => file.path)
+        });
+        const savedPin = await newPin.save();
+
         res.status(200).json(savedPin);
-    } catch(err){
-        res.status(500).json(err);
+    } catch (err) {
+        res.status(500).json({ error: "Internal Server Error" });
     }
 });
 
 // get all pins
 
-router.get("/",async(req,res)=>{
-    try{
+router.get("/", async (req, res) => {
+    try {
         const pins = await Pin.find();
-        res.status(200).json(pins) ;
-    } catch(err){
+        res.status(200).json(pins);
+    } catch (err) {
         res.status(500).json(err);
     }
 });
